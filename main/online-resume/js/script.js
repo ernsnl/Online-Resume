855d8b3f325a183e9d62eb9c2e77908b1dd59a66
ernsnl <ernsnl@gmail.com>
2016-04-20 19:03:40 +0300

    Fixed

--- main/online-resume/js/script.js
+++ main/online-resume/js/script.js
@@ -1,5 +1,6 @@
+'use strict';
 // Bio/Info HTML
-var htmlNameRole = "<h1>%data_n%</h1><span>%data_r%</span><hr/>"
+var htmlNameRole = "<h1>%data_n%</h1><span>%data_r%</span><hr/>";
 var htmlContact = "<ul class='contact-list'></ul>";
 var htmlContactGeneric = "<li><span class='contact-header'>%type% </span>%value%</li>";
 var htmlQuoteGeneric = "<p class='quote'>%quote% <span class='who'>-%who%</span></p>";
@@ -8,10 +9,8 @@ var htmlSkillGeneric = "<li>%data%</li>";
 var htmlProfileImg = "<img class='img img-responsive profileImg' alt='myPicture' src='%data%'/>";
 // Bio Data
 var bio = {
-    name_role: {
-        name: "Sinasi Eren Senel",
-        role: "Junior Software Developer/Student",
-    },
+    name: "Sinasi Eren Senel",
+    role: "Junior Software Developer/Student",
     contacts: {
         Mobile: "+905324862675",
         Email: "ernsnl@gmail.com",
@@ -19,27 +18,23 @@ var bio = {
         Twitter: "ernsnl",
         Location: "Annapolis, MD",
     },
-    quote: {
-        'Mahatma Gandhi': "Live as if you were to die tommorrow. Learn as if you were to live forever."
-    },
+    welcomeMessage: "Live as if you were to die tommorrow. Learn as if you were to live forever.",
+    welcomeMessageWho: 'Mahatma Gandhi',
     skills: ["C/C++", "C#", "SQL", "Javascript", "JQuery", "Java", "Python", "ASP.NET", "HTML", "Haskell"],
     biopic: "img/Profile.jpg",
     display: function(documentID) {
         var appendedSection = $("#" + documentID).find(".info-section");
-        appendedSection.find('.info-name-role').append(htmlNameRole.replace('%data_n%', this.name_role["name"]).replace('%data_r%', this.name_role["role"]));
+        appendedSection.find('.info-name-role').append(htmlNameRole.replace('%data_n%', this.name).replace('%data_r%', this.name));
         appendedSection.find('.info-contact').append(htmlContact);
         $.each(this.contacts, function(index_c, value_c) {
             appendedSection.find('.info-contact .contact-list').append(htmlContactGeneric.replace('%type%', index_c).replace('%value%', value_c));
         });
-        $.each(this.quote, function(index_c, value_c) {
-            appendedSection.find('.info-quote').append(htmlQuoteGeneric.replace('%quote%', value_c).replace('%who%', index_c));
-        });
+        appendedSection.find('.info-quote').append(htmlQuoteGeneric.replace('%quote%', this.welcomeMessage).replace('%who%', this.welcomeMessageWho));
         appendedSection.find('.info-skill').append(htmlSkill);
-        for (var i in this.skills) {
-            appendedSection.find('.skill-list').append(htmlSkillGeneric.replace('%data%', this.skills[i]));
-        }
+        this.skills.forEach(function(val) {
+            appendedSection.find('.skill-list').append(htmlSkillGeneric.replace('%data%', val));
+        });
         appendedSection.find('.info-image').append(htmlProfileImg.replace('%data%', this.biopic));
-
         // Let's Connect
         appendedSection = $("#" + documentID).find('.connectList').append(htmlContact);
         $.each(this.contacts, function(index_c, value_c) {
@@ -55,12 +50,12 @@ var education = {
         name: "Istanbul Technical University",
         location: "Istanbul",
         degree: "Bachelor's Degree",
-        major: "Computer Engineer",
+        major: ["Computer Engineer"],
         dates: "2016",
         url: "http://www.bb.itu.edu.tr/"
     }],
     onlineCourses: [{
-        name: "Java Summer Camp",
+        title: "Java Summer Camp",
         school: "OBSS",
         date: "2015",
         url: "http://obss.com.tr/academic/"
@@ -70,27 +65,28 @@ var education = {
         var appendedArea = $("#" + documentID).find('.education-duplicate').parent();
         var duplicate = $('#' + documentID).find('.education-duplicate');
         $('#' + documentID).find('.education-duplicate').remove();
-        for (var i in this.schools) {
+        this.schools.forEach(function(val) {
             var duplicate_new = duplicate.clone();
-            duplicate_new.find('.education-header .school-url').attr('href', this.schools[i].url);
-            duplicate_new.find('.education-header .school-url').append(this.schools[i].name + " - " +
-                this.schools[i].degree);
-            duplicate_new.find('.education-info .date').append(this.schools[i].dates);
-            duplicate_new.find('.education-info .location').append(this.schools[i].location);
-            duplicate_new.find('.education-major').append(this.schools[i].major);
+            duplicate_new.find('.education-header .school-url').attr('href', val.url);
+            duplicate_new.find('.education-header .school-url').append(val.name + " - " + val.degree);
+            duplicate_new.find('.education-info .date').append(val.dates);
+            duplicate_new.find('.education-info .location').append(val.location);
+            val.majors.forEach(function(maj) {
+                duplicate_new.find('.education-major').append(maj);
+            });
             appendedArea.append(duplicate_new);
-        }
+        });
         // Online Courses
         appendedArea = $("#" + documentID).find('.online-duplicate').parent();
         duplicate = $('#' + documentID).find('.online-duplicate');
         $('#' + documentID).find('.online-duplicate').remove();
-        for (var i in this.onlineCourses) {
+        this.onlineCourses.forEach(function(val) {
             var duplicate_new = duplicate.clone();
-            duplicate_new.find('.online-header .online-url').attr('href', this.onlineCourses[i].url);
-            duplicate_new.find('.online-header .online-url').append(this.onlineCourses[i].name + " - " + this.onlineCourses[i].school);
-            duplicate_new.find('.online-info .date').append(this.onlineCourses[i].date);
+            duplicate_new.find('.online-header .online-url').attr('href', val.url);
+            duplicate_new.find('.online-header .online-url').append(val.title + " - " + val.school);
+            duplicate_new.find('.online-info .date').append(val.date);
             appendedArea.append(duplicate_new);
-        }
+        });
     }
 };
 
@@ -115,27 +111,26 @@ var work = {
         var appendedArea = $("#" + documentID).find('.work-duplicate').parent();
         var duplicate = $("#" + documentID).find('.work-duplicate');
         $("#" + documentID).find('.work-duplicate').remove();
-        for (var i in this.jobs) {
+        this.jobs.forEach(function(val) {
             var duplicate_new = duplicate.clone();
-            duplicate_new.find('.work-header').append(this.jobs[i].employer + " - " +
-                this.jobs[i].title);
-            duplicate_new.find('.work-info .date').append(this.jobs[i].dates);
-            duplicate_new.find('.work-info .location').append(this.jobs[i].location);
-            duplicate_new.find('.work-summary').append(this.jobs[i].description);
+            duplicate_new.find('.work-header').append(val.employer + " - " +
+                val.title);
+            duplicate_new.find('.work-info .date').append(val.dates);
+            duplicate_new.find('.work-info .location').append(val.location);
+            duplicate_new.find('.work-summary').append(val.description);
             appendedArea.append(duplicate_new);
-        }
+        });
     }
 };
-
 // Project HTML
-var htmlImg = "<img class='projectImg img img-responsive' src='%data%' alt='projectImage'/>"
-    // Project Data
+var htmlImg = "<img class='projectImg img img-responsive' src='%data%' alt='projectImage'/>";
+// Project Data
 var projects = {
     projects: [{
         title: "Personal Website",
         dates: "2015",
         description: "Using Entity Framework and MVC, Personal Website was created.",
-        url:'http://ernsnl.com',
+        url: 'http://ernsnl.com',
         images: ["img/personalWebsite.png"]
     }, {
         title: "Fan Project",
@@ -148,23 +143,19 @@ var projects = {
         var appendedArea = $("#" + documentID).find('.project-duplicate').parent();
         var duplicate = $("#" + documentID).find('.project-duplicate');
         $("#" + documentID).find('.project-duplicate').remove();
-        for (var i in this.projects) {
+        this.projects.forEach(function(proj) {
             var duplicate_new = duplicate.clone();
-            duplicate_new.find('.project-header .project-url').attr('href',this.projects[i].url);
-            duplicate_new.find('.project-header .project-url').append(this.projects[i].title);
-            duplicate_new.find('.project-info .date').append(this.projects[i].dates);
-            duplicate_new.find('.project-summary').append(this.projects[i].description);
-            for (var j in this.projects[i].images) {
-                duplicate_new.find('.project-images').append(htmlImg.replace('%data%', this.projects[i].images[j]));
-            }
+            duplicate_new.find('.project-header .project-url').attr('href', proj.url);
+            duplicate_new.find('.project-header .project-url').append(proj.title);
+            duplicate_new.find('.project-info .date').append(proj.dates);
+            duplicate_new.find('.project-summary').append(proj.description);
+            proj.images.forEach(function(val) {
+                duplicate_new.find('.project-images').append(htmlImg.replace('%data%', val));;
+            });
             appendedArea.append(duplicate_new);
-        }
+        });
     }
 };
-
-var visited = ["San Antonio, TX", "Istanbul, Turkey", "Bern, Switzerland", "Chicago, IL", "Annapolis, MD", "Nurnberg, Germany", "Prague, Czech Republic", "Orlando, FL", "Washington, DC"];
-
-
 // Main Build Resume Funtion
 function buildResume(documentID, bio, education, work, projects) {
     if (!jQuery.isEmptyObject(bio)) {
